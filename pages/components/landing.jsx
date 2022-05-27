4cd58cd2189d5af935c59f0bb2b52f9232336f8b
YeaGram <94807715+YeaGram@users.noreply.github.com>
2022-05-27 11:19:43 +0800

    view

--- pages/components/landing.jsx
+++ pages/components/landing.jsx
@@ -1,8 +1,8 @@
 import ExploreButton from "./exploreBtn";
 const Landing = () => {
    return (
-      <div className="h-screen">
-         <div className="bg-Main bg-fixed w-screen h-full bg-origin-content bg-[length:auto_100%] bg-center grid place-items-center relative after:absolute after:block after:inset-0 after:bg-gradient-to-t after:from-rose-900 after:to-transparent">
+      <div className="h-screen mt-14">
+         <div className="bg-Main mt-6 w-full h-full bg-cover bg-center grid place-items-center relative after:absolute after:block after:inset-0 after:bg-gradient-to-t after:from-rose-900 after:to-transparent">
             <div className="w-[80%] h-full grid place-items-center z-10 place-content-center sm:w-auto">
                <h3 className="font-montserrat font-extrabold text-4xl text-transparent drop-shadow-myDrop1 bg-gradient-to-b to-red-800 via-rose-500 from-rose-200 bg-clip-text">
                   Hello There!
